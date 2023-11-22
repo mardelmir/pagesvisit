664e9a4be8affcda59a0d7f79fb4a63e1d45d478
María Delgado <mariad1996@gmail.com>
2023-11-22 19:40:49 +0100

    finished js, need to polish css

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const texto = document.getElementById('contadorVisitas')
+const btn = document.getElementById('btnReestablecer')
+
+window.addEventListener('load', () => {
+
+    !localStorage.contador
+        ? localStorage.setItem('contador', '1')
+        : localStorage.contador = parseFloat(localStorage.getItem('contador')) + 1;
+
+    texto.innerText = localStorage.contador;
+})
+
+btn.addEventListener('click', () => {
+    localStorage.clear()
+    texto.textContent = 0
+})
+
