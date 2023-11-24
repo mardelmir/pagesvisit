73692110cfa9b0293eb26d16cbf84ea35e4a8039
María Delgado <mariad1996@gmail.com>
2023-11-24 18:36:14 +0100

    finished

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const texto = document.getElementById('contadorVisitas')
 const btn = document.getElementById('btnReestablecer')
 
-window.addEventListener('load', () => {
-    !localStorage.contador
-        ? localStorage.setItem('contador', '1')
-        : localStorage.contador = parseFloat(localStorage.getItem('contador')) + 1;
-    texto.innerText = localStorage.contador;
-})
+
+!localStorage.contador
+    ? localStorage.setItem('contador', '1')
+    : localStorage.contador = parseFloat(localStorage.getItem('contador')) + 1;
+texto.innerText = localStorage.contador;
+
 
 btn.addEventListener('click', () => {
     localStorage.clear()
